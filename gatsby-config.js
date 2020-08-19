52504b72d4f6d8986b1656854b84e12996070204
rjgoetz <rj@rjgoetz.com>
2020-08-18 20:15:02 -0500

    made favicon

--- gatsby-config.js
+++ gatsby-config.js
@@ -18,13 +18,13 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: `gatsby-starter-default`,
-        short_name: `starter`,
+        name: `Ahlberg For Edina City Council`,
+        short_name: `Ahlberg For Council`,
         start_url: `/`,
-        background_color: `#663399`,
-        theme_color: `#663399`,
+        background_color: `#1b5633`,
+        theme_color: `#1b5633`,
         display: `minimal-ui`,
-        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+        icon: `src/images/ahlberg-icon.png`, // This path is relative to the root of the site.
       },
     },
     'gatsby-plugin-netlify',
